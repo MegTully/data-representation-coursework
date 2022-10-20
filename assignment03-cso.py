16cea966c2fb458839e6a43e33f0593cac26fe83
MegTully <g00411235@gmit.ie>
2022-10-20 18:28:30 +0100

    Added in comments

--- assignment03-cso.py
+++ assignment03-cso.py
@@ -1,13 +1,17 @@
+#import packages
 from ast import Pass
 import requests
 import json
 
+#url that links to the exchequer account (historical series) page on cso.ie
 Url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en/JSON-stat/2.0/en"
 
+#function to acess the url and returns it as a json
 def getAll():
     response = requests.get(Url)
     return response.json()
 
+#function that creates a json file and puts in the data which is formated as a json
 if __name__ == "__main__" :
     with open("cso.json", "wt") as fp: 
         print(json.dumps(getAll()), file = fp)
